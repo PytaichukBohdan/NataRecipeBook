6a2e9b3b255eeb36343619d8e375835b1c4675c9
pytaichukbohdan <woomniazhko@gmail.com>
2026-03-17 13:37:19 +0000

    feat: all breakfast videos slowed down 1.5x for smooth viewing

--- lib/recipe-loader.ts
+++ lib/recipe-loader.ts
@@ -2,7 +2,7 @@ import type { RecipeData } from "@/types/recipe"
 import recipesData from "@/data/recipes.json"
 
 // Cache-busting version - increment when updating images/videos
-const ASSET_VERSION = "v7"
+const ASSET_VERSION = "v8"
 
 /**
  * Loads and validates recipe data from recipes.json
