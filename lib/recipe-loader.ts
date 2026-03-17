31145b15665c14f98016e2464bebe8f9a2bb9569
pytaichukbohdan <woomniazhko@gmail.com>
2026-03-17 13:01:59 +0000

    feat: breakfast_5 no spoon video + cache v5

--- lib/recipe-loader.ts
+++ lib/recipe-loader.ts
@@ -2,7 +2,7 @@ import type { RecipeData } from "@/types/recipe"
 import recipesData from "@/data/recipes.json"
 
 // Cache-busting version - increment when updating images/videos
-const ASSET_VERSION = "v3"
+const ASSET_VERSION = "v5"
 
 /**
  * Loads and validates recipe data from recipes.json
